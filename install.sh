56e9e6e033a3207bf5af75296e6cbede54022cf9
ShogoAsada <asapontenhou@gmail.com>
2019-11-06 20:58:38 +0900

    基本処理のスクリプト追加

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+bin/setup
+
+Homebrew does not allow sudo.
+case "$(uname)" in
+  "Darwin")  bin/mitamae local $@ lib/recipe.rb ;;
+  *) sudo -E bin/mitamae local $@ lib/recipe.rb ;;
+esac
+
+echo "Completed!"
+
