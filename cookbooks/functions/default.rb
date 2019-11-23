d478d035e8867591bd83f59a0f0e2da3d1dd439e
ShogoAsada <asapontenhou@gmail.com>
2019-11-23 13:57:10 +0900

    nvimのリンクの貼り方を変えた

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -1,12 +1,3 @@
-define :ln do
-  name = params[:name]
-  link node[:xdg_config_home] do
-    to File.expand_path("../../#{name}/files/#{name}", __FILE__)
-    user node[:user]
-    force true
-  end
-end
-
 define :tap do
   execute "brew tap #{params[:name]}" do
     user, repository = params[:name].split('/')
--- cookbooks/nvim/default.rb
+++ cookbooks/nvim/default.rb
@@ -1,2 +1,6 @@
-ln 'nvim'
+link node[:xdg_config_home] do
+  to File.expand_path('../../nvim/files/nvim', __FILE__)
+  user node[:user]
+  force true
+end
 
