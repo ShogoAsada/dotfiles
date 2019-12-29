c6d9ae574693ca00fe63a139e5b2f7b7123c124c
ShogoAsada <asapontenhou@gmail.com>
2019-12-29 15:18:58 +0900

    マシン環境別のgitconfig.local設定追加

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -4,6 +4,12 @@ link File.expand_path("#{node[:home]}/.gitconfig") do
   force true
 end
 
+template File.expand_path("#{node[:home]}/.gitconfig.local") do
+  source File.expand_path('../templates/.gitconfig.local', __FILE__)
+  owner node[:user]
+  not_if "test -f #{node[:home]}/.gitconfig.local"
+end
+
 link File.expand_path("#{node[:home]}/.git_commit_msg_tpl") do
   to File.expand_path('../files/.git_commit_msg_tpl', __FILE__)
   user node[:user]
