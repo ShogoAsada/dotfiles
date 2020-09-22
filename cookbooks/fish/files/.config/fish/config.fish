64b6180261b3458309b6692c6dd14eada12df29f
ShogoAsada <asapontenhou@gmail.com>
2020-09-22 17:32:37 +0900

    :bug: fix: nodenvのパスを先頭にする

--- cookbooks/fish/files/.config/fish/config.fish
+++ cookbooks/fish/files/.config/fish/config.fish
@@ -3,7 +3,6 @@ set -x PATH $HOME/.rbenv/shims $PATH
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x GHQ_ROOT $HOME/ghq/src
 eval (direnv hook fish)
-set -x PATH $HOME/.nodenv/shims $PATH
 set -x FZF_DEFAULT_OPTS '--height 40% --reverse --border'
 set -U FZF_LEGACY_KEYBINDINGS 0
 
@@ -35,3 +34,4 @@ remove_duplicate_path
 # /usr/loca/bin を先頭にするために
 # remove_duplicate_path を修正する必要あり
 set -x PATH /usr/local/bin $PATH
+set -x PATH $HOME/.nodenv/shims $PATH
