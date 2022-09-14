3116a110dee33700ee598929018d4f17fbe6fe3e
ShogoAsada <asapontenhou@gmail.com>
2022-09-14 15:02:52 +0900

    feat: Remove cookbooks

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -4,11 +4,9 @@ include_cookbook 'homebrew'
 
 include_cookbook 'fish'
 include_cookbook 'git'
-include_cookbook 'gcc'
 include_cookbook 'nodenv'
 include_cookbook 'nvim'
 include_cookbook 'rbenv'
 include_cookbook 'python3'
-include_cookbook 'python2'
 include_cookbook 'tfenv'
 
